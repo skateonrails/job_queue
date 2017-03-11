2391e71afdd305e181ee6e5b0d63220f6d45e69a
Samuel Prado <darkms@gmail.com>
2017-03-10 21:02:14 -0300

    Make JobFile class import just 80% of jobs from file

--- app/job_file.rb
+++ app/job_file.rb
@@ -1,19 +1,29 @@
 # frozen_string_literal: true
 
-# JobFile is responsible to parse a txt file into JobLine objects
+# JobFile is responsible to parse a txt file into JobLine objects.
+# According to the challenge, it must import just 80% of job lines
+# found in file.
 class JobFile
   include Enumerable
   extend Forwardable
 
   HEADER = 'partnerId|title|categoryId|ExpiresAt'
+  IMPORT_PERCENTAGE = 0.8
 
   def initialize(filepath: nil)
     @filepath = filepath
     @lines = []
-    parse_lines
   end
 
-  def_delegators :lines, :each, :first
+  def_delegators :lines, :each, :first, :size
+
+  def parse
+    lines_to_import.each do |file_line|
+      lines << JobLine.new(input: file_line)
+    end
+
+    self
+  end
 
   def expires_at_greater_than(date)
     lines.find_all { |job_line| job_line.expires_at > date }
@@ -23,16 +33,25 @@ class JobFile
 
   attr_accessor :filepath, :lines
 
-  def parse_lines
-    file.each_line do |file_line|
-      sanitized_line = file_line.chomp
-
-      next if sanitized_line == HEADER
-      lines << JobLine.new(input: sanitized_line)
-    end
+  def file
+    @file ||= File.new(filepath)
   end
 
-  def file
-    File.new(filepath)
+  def file_lines
+    @file_lines ||= file.readlines
+  end
+
+  def sanitized_lines
+    @sanitized_lines ||= file_lines.map(&:chomp)
+  end
+
+  def job_lines
+    @job_lines ||= sanitized_lines.reject { |line| line == HEADER }
+  end
+
+  def lines_to_import
+    total = job_lines.size
+    last_element_index = (total * IMPORT_PERCENTAGE).ceil
+    job_lines[0...last_element_index].to_a
   end
 end
--- spec/app/job_file_spec.rb
+++ spec/app/job_file_spec.rb
@@ -5,28 +5,46 @@ describe JobFile do
   let(:filepath) { File.join(File.dirname(__FILE__), %w(.. file jobs_for_test.txt)) }
   let(:job_file) { JobFile.new(filepath: filepath) }
 
-  it 'should act as an array of JobLine objects' do
-    job_file.each do |object|
-      expect(object).to respond_to(:partner_id)
-      expect(object).to respond_to(:title)
-      expect(object).to respond_to(:category_id)
-      expect(object).to respond_to(:expires_at)
-      expect(object).to respond_to(:to_json)
+  describe '#parse' do
+    before :each do
+      job_file.parse
+    end
+
+    it 'should act as an array of JobLine objects' do
+      job_file.each do |object|
+        expect(object).to respond_to(:partner_id)
+        expect(object).to respond_to(:title)
+        expect(object).to respond_to(:category_id)
+        expect(object).to respond_to(:expires_at)
+        expect(object).to respond_to(:to_json)
+      end
+    end
+
+    it 'should not parse the header of file' do
+      first_line = job_file.first
+      expect(first_line.partner_id).to eq(1123)
+      expect(first_line.title).to eq('Vendedor')
+      expect(first_line.category_id).to eq(3)
+      expect(first_line.expires_at).to eq(Date.parse('21/1/2017'))
+    end
+
+    it 'should get only 80% of the jobs from file' do
+      lines_size = File.open(filepath, 'r').readlines.size - 1 #removing header
+      total_imported_lines = (lines_size * 0.8).ceil
+      expect(job_file.size).to eq(total_imported_lines)
     end
   end
 
-  it 'should not parse the header of file' do
-    first_line = job_file.first
-    expect(first_line.partner_id).to eq(1123)
-    expect(first_line.title).to eq('Vendedor')
-    expect(first_line.category_id).to eq(3)
-    expect(first_line.expires_at).to eq(Date.parse('21/1/2017'))
-  end
-
   describe '#expires_at_greater_than' do
+    it 'should return an empty array if does not find lines with condition' do
+      date = Date.parse('20/12/2019')
+      lines = job_file.parse.expires_at_greater_than(date)
+      expect(lines).to be_empty
+    end
+
     it 'should return lines that expires_at is greater than date passed as argument' do
       date = Date.parse('20/12/2017')
-      lines = job_file.expires_at_greater_than(date)
+      lines = job_file.parse.expires_at_greater_than(date)
       expect(lines.first.partner_id).to eq(4435)
     end
   end
