646ab013816b5dab649188877ab39388743eab73
Samuel Prado <darkms@gmail.com>
2017-03-11 18:30:56 -0300

    Update Sneakers config

--- config/sneakers_config.rb
+++ config/sneakers_config.rb
@@ -4,5 +4,13 @@ require 'logger'
 
 amqp_address = ENV['AMQP_ADDRESS'] || 'amqp://guest:guest@localhost:5672'
 
-Sneakers.configure log: STDOUT, amqp: amqp_address
+config = {
+  durable: true,
+  heartbeat: 2,
+  exchange: 'sneakers',
+  log: STDOUT,
+  amqp: amqp_address
+}
+
+Sneakers.configure config
 Sneakers.logger.level = Logger::INFO
